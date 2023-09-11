ae5b420debe5ff627d5eca4cb898777036904f33
platisam <platisam@gmail.com>
2023-09-11 12:19:24 +0200

    setting url and states in app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <div className="App">Tabs Project</div>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  return <h2>Tabs Project Setup</h2>;
 }
 
 export default App;
